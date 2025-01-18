451725766a49efaad5c4e99a627e69032b579434
b1san <b1.san.teru@gmail.com>
2025-01-18 10:19:17 +0900

    共通エラー処理の追加
    ・@ResponseBody変換エラー時のレスポンス情報の設定処理を追加
    ・ハンドラー未定義例外用のレスポンス情報の設定処理を追加

--- src/main/kotlin/com/example/library/config/ErrorResponseEntityExceptionHandler.kt
+++ src/main/kotlin/com/example/library/config/ErrorResponseEntityExceptionHandler.kt
@@ -1,8 +1,12 @@
 package com.example.library.config
 
 import com.example.library.exception.CustomResponseStatusException
+import com.example.library.model.error.ErrorResponse
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
@@ -11,8 +15,48 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 class ErrorResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
 
+    /**
+     * @ResponseBody変換エラー時に発生するHttpMessageNotReadableExceptionのハンドラ
+     *
+     * @param ex 例外
+     * @param headers レスポンスヘッダー
+     * @param status HTTPステータス
+     * @param request リクエスト
+     * @return レスポンス情報
+     */
+    override fun handleHttpMessageNotReadable(
+        ex: HttpMessageNotReadableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        val body = ErrorResponse("パラメーターの型が一致していません。", null)
+        return this.handleExceptionInternal(ex, body, headers, status, request)
+    }
+
+    /**
+     * 論理エラー用CustomResponseStatusExceptionのハンドラー
+     *
+     * @param ex 例外
+     * @param request リクエスト
+     * @return レスポンス情報
+     */
     @ExceptionHandler(CustomResponseStatusException::class)
     fun handleCustomResponseStatusException(ex: CustomResponseStatusException, request: WebRequest): ResponseEntity<Any>? {
         return handleExceptionInternal(ex, ex.error, HttpHeaders(), ex.status, request)
+    }
+
+    /**
+     * ハンドラー未定義例外用のハンドラー
+     *
+     * @param ex 例外
+     * @param request リクエスト
+     * @return レスポンス情報
+     */
+    @ExceptionHandler(Exception::class)
+    fun handleAll(ex: Exception, request: WebRequest): ResponseEntity<Any> {
+        //TODO スタックトレースのログ出力
+        val body = ErrorResponse("予期せぬエラーが発生しました。", null)
+        return ResponseEntity<Any>(body, HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR)
     }
 }
